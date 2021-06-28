8c2e601a827165dfcc2e0f6333c3f37bb2b93d25
Mateusz Serafin <mateusz97.serafin@gmail.com>
2021-06-28 13:54:06 +0200

    created DataBase class

---
+++ DataBase.hpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include "Student.hpp"
+
+class DataBase
+{
+private:
+    std::vector<Student>Students;
+    bool validPesel(std::string pesel);
+    int charToInt(char z);
+
+public:
+
+    bool addStudent(std::string firstName, std::string lastName, std::string address,
+             int indexNumber, std::string pesel, Sex sex);
+
+    void display();
+    std::vector<const Student *> searchLastName();
+    Student* searchPesel(std::string pesel);
+    void sortByPesel();
+    void sortByLastName();
+    void remove(std::string indexNumber);
+    void saveDataBase();
+    void loadDataBase();
+    int size() {return Students.size();}
+    Student& operator[](int i) {return Students[i];}
+};
